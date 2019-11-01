d070a2afadf530d561ad13e5827f1bc64493b224
Alejandro Rayas <alexrayas@rocketmail.com>
2019-11-01 16:06:52 -0700

    complete

--- game.js
+++ game.js
@@ -1,14 +1,19 @@
 //alert("fuck you");
-
+var wrong = 0;
+var toggle = false;
+var level = 0;
 var userClickedPattern = [];
 var gamePattern = [];
 var buttonColors = ["red", "blue", "green", "yellow"];
 function nextSquence(){
+    userClickedPattern = [];
+    $("#level-title").text = ("level" + level);
     var randomNumber = Math.floor((Math.random() * 4)); // gens random num from 0 to 3
     var randomChosenColor = buttonColors[randomNumber];
     gamePattern.push(randomChosenColor);
     $("#" + randomChosenColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
     playSound(randomChosenColor);
+    level++;
 
     
 }
@@ -17,11 +22,12 @@ $(".btn").on("click", function(){
     userClickedPattern.push(userChosenColor);
     playSound(userChosenColor);
     animatePress(userChosenColor);
+    checkAnswer(userClickedPattern.length - 1);
     console.log(userClickedPattern);
 });
 
 function playSound(name) {
-    var audio = new Audio("sounds/" + name + ".mp3")
+    var audio = new Audio("sounds/" + name + ".mp3");
     audio.play();
     
 }
@@ -33,4 +39,59 @@ function animatePress(currentColor) {
     setTimeout(function(){
     $("#" + currentColor).removeClass("pressed");
     }, delayInMilliseconds);
+}
+
+$(document).keypress(function(event){
+    
+    if(!toggle){
+        if(event.key == "a" || event.key == "A"){ 
+            console.log(toggle);
+            $("#level-title").text("Level " + level);
+            nextSquence();     
+            toggle = true;       
+        }
+    }
+
+    if(wrong == 1){
+        $("#level-title").text("Level " + level);
+        nextSquence();     
+        wrong = 0;
+    }
+
+    
+});
+
+function checkAnswer(currentLevel) {
+    if(gamePattern[currentLevel] === userClickedPattern[currentLevel]){
+        console.log("success");
+        var delayInMilliseconds = 1000; //1 second
+        if(userClickedPattern.length === gamePattern.length){
+            setTimeout(function(){
+                nextSquence();
+            }, delayInMilliseconds);
+        }
+    }
+    else{
+        playSound("wrong");
+        $("body").addClass("game-over");
+        var delayInMilliseconds = 200; //.2 second
+        setTimeout(function(){
+            $("body").removeClass("game-over");
+        }, delayInMilliseconds);
+
+        $("#level-title").text("Game Over, Press Any Key to Restart");
+
+        startOver();
+        console.log("wrong");
+    }
+    
+}
+
+function startOver() {
+    wrong = 1;
+    gamePattern = [];
+    level = 0;
+    userClickedPattern = [];
+    toggle = false;
+    
 }
